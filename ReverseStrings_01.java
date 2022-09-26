0becdd8aa845e60adf032d4064ac3c2909a6e023
NikolaYordanov94 <106193855+NikolaYordanov94@users.noreply.github.com>
2022-09-26 23:16:45 +0300

    Add files via upload

---
+++ ReverseStrings_01.java
@@ -0,0 +1,25 @@
+package TextProcessing;
+
+import java.util.Scanner;
+
+public class ReverseStrings_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        while(!text.equals("end")){
+            String reversed = "";
+
+            for (int i = text.length() - 1; i >= 0; i--) {
+
+                char symbol = text.charAt(i);
+                reversed += symbol;
+            }
+
+            System.out.printf("%s = %s%n", text, reversed);
+
+            text = scanner.nextLine();
+        }
+    }
+}
